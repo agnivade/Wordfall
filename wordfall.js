7546172409667c543f7eac3ae344554b138d7b1d
Agniva De Sarker <adesarkar@instartlogic.com>
2013-08-25 01:00:20 +0530

    Converting the code into a jQuery module
    Reviewed by:
    Testing:
    Tickets:
    Backport:

--- wordfall.js
+++ wordfall.js
@@ -4,49 +4,65 @@ function divideTextInSpans(text){
    }).join(" ");
 }
 
-$(document).ready(function(){
-	var style = document.documentElement.
-                appendChild(document.createElement("style"));
-	var newInnerHtml = divideTextInSpans($("#block").text());
-  $("#block").html(newInnerHtml);
-	$("#block").children().each(function(count){
-		var index = Modernizr._prefixes.length;
-		var rule = "";
-		var elem = $(this);
-		var offset = $(elem).offset();
-		var targetTop = $(document).height() - offset.top - 20;
-		var targetLeft = offset.left + 20;
-
-		while(index--){
-      rule="@"+Modernizr._prefixes[index]+"keyframes wordfall"+count+"\
-      { from{ \
-	      top:"+offset.top+"px; \
-	      left:"+offset.left+"px; \
-	      } \
-	      to{ \
-	      top:"+targetTop+"px; \
-	      left:"+targetLeft+"px; \
-	      -webkit-transform:rotate(720deg); \
-	      -moz-transform:rotate(720deg); \
-	      -transform:rotate(720deg); \
-	      } \
-      } ";
-      try {
-        style.sheet.insertRule(rule);
-      } catch(err){}
-		}
-		var interval = (count+1) * 1000;
-		setTimeout(function(){
-			setAnimation(elem, count)
-		},interval);
-
-	});
-
-});
-
 function setAnimation(elem, count) {
-	$(elem).css("position", "absolute");
-	$(elem)[0].style[Modernizr.prefixed('animation')] = "wordfall"+count+" 1s";
-	$(elem)[0].style[Modernizr.prefixed('animation-fill-mode')] = "forwards";
+  $(elem).css("position", "absolute");
+  $(elem)[0].style[Modernizr.prefixed('animation')] = "wordfall"+count+" 1s";
+  $(elem)[0].style[Modernizr.prefixed('animation-fill-mode')] = "forwards";
 }
 
+
+(function ($) {
+  $.fn.wordFall = function(options) {
+    var settings = $.extend({
+      'paddingRight':20,
+      'paddingBottom':20
+    }, options);
+    /* Internal settings */
+    var multiplier = 360;
+    var adder = 720;
+
+    var style = document.documentElement.
+                  appendChild(document.createElement("style"));
+    return this.each(function(){
+      var newInnerHtml = divideTextInSpans($(this).text());
+      $(this).html(newInnerHtml);
+      $(this).children().each(function(count){
+        var rotateDegree = Math.random()*multipler + adder;
+        var index = Modernizr._prefixes.length;
+        var rule = "";
+        var elem = $(this);
+        var offset = $(elem).offset();
+        var targetTop = $(document).height()
+                          - offset.top
+                          - settings.paddingBottom;
+        var targetLeft = offset.left + settings.paddingRight;
+
+        while(index--){
+
+          rule="@"+Modernizr._prefixes[index]+"keyframes wordfall"+count+"\
+          { from{ \
+            top:"+offset.top+"px; \
+            left:"+offset.left+"px; \
+            } \
+            to{ \
+            top:"+targetTop+"px; \
+            left:"+targetLeft+"px; \
+            -webkit-transform:rotate("+rotateDegree+"deg); \
+            -moz-transform:rotate("+rotateDegree+"deg); \
+            -transform:rotate("+rotateDegree+"deg); \
+            } \
+          } ";
+          try {
+            style.sheet.insertRule(rule);
+          } catch(err){}
+        }
+        var interval = (count+1) * 1000;
+        setTimeout(function(){
+          setAnimation(elem, count)
+        },interval);
+
+      });
+
+    });
+  };
+})(jQuery);
